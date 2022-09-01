07646fb45ca93ea506556691010f4974f7776e04
Kaspar Birk <kaspar.birk@gmail.com>
2022-09-01 16:10:18 +0200

    reordered methods

--- slice.go
+++ slice.go
@@ -11,28 +11,6 @@ type Comparable[T any] interface {
 	CompareTo(other T) int
 }
 
-func search[T any](slice []T, element T, compare func(a, b T) int) int {
-	start := 0
-	end := len(slice) - 1
-	middle := (start + end) / 2
-
-	for compare(slice[middle], element) != 0 && start <= end {
-		if compare(slice[middle], element) >= 1 {
-			start = middle + 1
-		} else {
-			end = middle - 1
-		}
-
-		middle = (start + end) / 2
-	}
-
-	if compare(slice[middle], element) == 0 {
-		return middle
-	}
-
-	return -1
-}
-
 // ComparableSlice takes an ordered slice of comparable elements (Comparable[T]) and the element to search for,
 // and performs a binary search on the slice.
 // It returns the index of given element, if found, or otherwise -1
@@ -57,4 +35,27 @@ func Slice[T constraints.Ordered](slice []T, element T) int {
 
 		return 0
 	})
+}
+
+// search uses binary search to find the index of the given element
+func search[T any](slice []T, element T, compare func(a, b T) int) int {
+	start := 0
+	end := len(slice) - 1
+	middle := (start + end) / 2
+
+	for compare(slice[middle], element) != 0 && start <= end {
+		if compare(slice[middle], element) >= 1 {
+			start = middle + 1
+		} else {
+			end = middle - 1
+		}
+
+		middle = (start + end) / 2
+	}
+
+	if compare(slice[middle], element) == 0 {
+		return middle
+	}
+
+	return -1
 }
